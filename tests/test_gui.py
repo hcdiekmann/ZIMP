588c41f4928453215957ab61f580b4bb04e99063
hcdiekmann <hcdiekmann@hotmail.com>
2023-11-19 18:31:04 +1300

    update test

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -38,16 +38,9 @@ class TestGUI(unittest.TestCase):
         self.assertEqual(self.gui.lable_health.cget("text"), "Health: 5")
         self.assertEqual(self.gui.lable_attack.cget("text"), "Attack: 2")
         self.assertEqual(self.gui.items.cget("text"), "Items: ['Chainsaw']")
-        self.assertEqual(self.gui.player_row, 1)
-        self.assertEqual(self.gui.player_col, 2)
-
-    def test_move_player(self):
-        self.gui._move_player(1, 2)
-        self.assertEqual(self.gui.player_row, 1)
-        self.assertEqual(self.gui.player_col, 2)
 
     def test_player_marker_color(self):
-        self.gui._draw_player()
+        self.gui._draw_player(row=1, col=2)
         marker_color = self.gui.canvas.itemcget(self.gui.player_marker, "fill")
         self.assertEqual(marker_color, "red")
 
